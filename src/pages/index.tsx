93d8c712b4a8e92b394a0ba223eb6aa7e13bf4fc
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-21 19:58:25 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
-import { FC } from "react";
 
-const HomePage: FC = () => {
-	return (
-		<>
-			<div className="bg-white w-full h-full text-black">
-				Test Pierwszego deploymentu
-			</div>
-		</>
-	);
-};
+      import React from 'react';
+      import Link from 'next/link';
 
-export default HomePage;
+      const GeneratedPage: React.FC = () => {
+        return (
+          <div>
+            <section className="h-[92.3vh]" style={{ position: "relative", backgroundColor: "#ac0c0c" }}>
+          <nav className="flex items-center justify-between p-4 h-[23.182481751824817%]" style={{ backgroundColor: "#333333" }}>
+          <div className="flex gap-4 items-center"><img src="https://9.allegroimg.com/s1024/0cbde8/0f4e067c4432bfe3642da1782279" alt="logo" className="w-[200px] h-[100px] ml:[5%] object-fill" /></div>
+<div className="flex gap-4 items-center"><Link href="/">main</Link> <Link href="/lubidzieci">lubidzieci</Link></div>
+        </nav>
+        </section>
+          </div>
+        );
+      };
+  
+      export default GeneratedPage;
+    
